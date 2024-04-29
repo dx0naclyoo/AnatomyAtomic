869e50e4ed869b51946ae76ad23df1872139349d
dx0naclyoo <konyzhenkovdanil@gmail.com>
2024-04-29 12:49:54 +0300

    Fix endpoint's

--- anatomic/Backend/Section/api.py
+++ anatomic/Backend/Section/api.py
@@ -7,9 +7,11 @@ from anatomic.tools import SortedMode
 router = APIRouter(tags=["Section"])
 
 
-@router.get("/sections/{section_id}", response_model=model.Section, name="section:get_by_id")
+@router.get(
+    "/sections/{section_id}", response_model=model.Section, name="section:get_by_id"
+)
 async def get_section_by_id(
-        section_id: int, service: SectionService = Depends(SectionService)
+    section_id: int, service: SectionService = Depends(SectionService)
 ):
     return await service.get_section_by_id(section_id)
 
@@ -21,14 +23,14 @@ async def get_section_by_id(
     limit - Число записей, что вернётся, max = 100.
     offset - смещение записей, пример offset=10: response: item_id - ..... 10, 11, 12, 13.
     group_by - Ещё НЕ РЕАЛИЗОВАНО, групировка по параметрам.
-    """
+    """,
 )
 async def get_all_section(
-        request: Request,
-        limit: int = 10,
-        offset: int = 0,
-        sorted_mode: SortedMode = SortedMode.ID,
-        service: SectionService = Depends(SectionService),
+    request: Request,
+    limit: int = 10,
+    offset: int = 0,
+    sorted_mode: SortedMode = SortedMode.ID,
+    service: SectionService = Depends(SectionService),
 ):
     if limit > 100:
         limit = 100
@@ -39,22 +41,22 @@ async def get_all_section(
 
 @router.post("/section/")
 async def create_section(
-        section: model.SectionCreate, service: SectionService = Depends(SectionService)
+    section: model.SectionCreate, service: SectionService = Depends(SectionService)
 ):
     return await service.create_section(section)
 
 
 @router.put("/section/{section_id}")
 async def update_section(
-        section_id: int,
-        section: model.SectionUpdate,
-        service: SectionService = Depends(SectionService),
+    section_id: int,
+    section: model.SectionUpdate,
+    service: SectionService = Depends(SectionService),
 ):
     return await service.update_section(section_id, section)
 
 
 @router.delete("/section/{section_id}")
 async def delete_section(
-        section_id: int, service: SectionService = Depends(SectionService)
+    section_id: int, service: SectionService = Depends(SectionService)
 ):
     return await service.delete_section(section_id)
