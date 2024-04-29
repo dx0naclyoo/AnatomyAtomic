0bb9968268b4b12212648e353b52a470c20787f6
dx0naclyoo <konyzhenkovdanil@gmail.com>
2024-04-29 20:20:42 +0300

    ADD Cors

--- anatomic/app.py
+++ anatomic/app.py
@@ -1,9 +1,45 @@
-from fastapi import FastAPI, Response
-from fastapi import Request
+from typing import Callable
+
+from fastapi import FastAPI, Response, APIRouter, Request
+from fastapi.routing import APIRoute
+
+from starlette.middleware.cors import CORSMiddleware
+
 from anatomic.Backend import router
 
 app = FastAPI()
 app.include_router(router)
+
+
+# Handle CORS
+class CORSHandler(APIRoute):
+    def get_route_handler(self) -> Callable:
+        original_route_handler = super().get_route_handler()
+
+        async def preflight_handler(request: Request):
+            if request.method == 'OPTIONS':
+                response = Response()
+                response.headers['Access-Control-Allow-Origin'] = '*'
+                response.headers['Access-Control-Allow-Methods'] = 'POST, GET, DELETE, OPTIONS'
+                response.headers['Access-Control-Allow-Headers'] = 'Authorization, Content-Type'
+            else:
+                response = await original_route_handler(request)
+
+        return preflight_handler
+
+
+router = APIRouter(route_class=CORSHandler)
+app.include_router(router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# ============================================
 
 
 @app.get("/")
